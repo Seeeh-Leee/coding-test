fa137be39413f91a26898c6e02bb0741d3c7221e
Seeeh-Leee <116495990+Seeeh-Leee@users.noreply.github.com>
2023-01-13 16:53:33 +0900

    Update dividedIntArray.go

--- dividedIntArray.go
+++ dividedIntArray.go
@@ -4,9 +4,31 @@ pakage main
 
 import{
   "fmt"
+  "sort"
 }
 
 func main {
+  var arr []int{5, 9, 7, 10}
+  divisor := 5
   
+  result := solution(arr,divisor)
+  fmt.Println(result) //[5,10]
+  
+}
+
+func solution(arr []int, divisor int) []int {
+    var result []int
+    
+    for _,value :=  range arr{
+        if value % divisor==0 {
+            result = append(result, value)
+        }
+    }
+    if len(result)==0 {
+        result = append(result, -1)
+    }
+    sort.Ints(result)
+    
+    return result
 }
 
