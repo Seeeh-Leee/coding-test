f9d44716c5a79790fe951d4d8159bfecedac6f7c
Seeeh-Leee <116495990+Seeeh-Leee@users.noreply.github.com>
2023-01-31 20:25:16 +0900

    Update 올바른 괄호.cpp

--- 올바른 괄호.cpp
+++ 올바른 괄호.cpp
@@ -2,6 +2,7 @@
 
 #include <string>
 #include <iostream>
+#include <stack>
 
 using namespace std;
 
@@ -9,13 +10,26 @@ bool solution(string s)
 {
     bool answer = true;
     int count=0;
+    stack<char> stack;
     
+// #1 using stack
     for (int i=0; i<s.length(); i++){
-      (s[i]=='(') ? count++ : count--;
-      if (count < 0 && s[i]==')') return false;
+    	if (s[i]=='(') stack.push(s[i]);
+        else {
+            if (!stack.empty())
+                stack.pop();
+            else return false;
+        }
     }
-    if (count != 0) answer = false;
+    answer = stack.empty();
+    
+// #2 using for loop
+    
+//     for (int i=0; i<s.length(); i++){
+//       (s[i]=='(') ? count++ : count--;
+//       if (count < 0 && s[i]==')') return false;
+//     }
+//     if (count != 0) answer = false;
     
     return answer;
 }
-
