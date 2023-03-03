4360b841ef62cdfd2de52b68fb478336f92a5869
Seeeh-Leee <116495990+Seeeh-Leee@users.noreply.github.com>
2023-03-03 16:59:54 +0900

    Create 분수의 덧셈.cpp with gcd code

---
+++ 분수의 덧셈.cpp
@@ -0,0 +1,42 @@
+//link : https://school.programmers.co.kr/learn/courses/30/lessons/120808 
+
+#include <string>
+#include <vector>
+
+using namespace std;
+int gcdf(int big, int small);
+
+vector<int> solution(int numer1, int denom1, int numer2, int denom2) {
+    vector<int> answer;
+    int n0, d0, gcd =0;
+
+    n0 = numer1 * denom2 + numer2 * denom1;
+    d0 = denom1 * denom2;
+    
+    if (d0 > n0) gcd = gcdf(d0,n0);
+    else gcd = gcdf(n0,d0);    
+    
+    
+    if (gcd != 0){
+        n0 = n0 / gcd;
+        d0 = d0 / gcd;
+    }
+    
+    answer.push_back(n0); 
+    answer.push_back(d0); 
+        
+    return answer;
+}
+
+int gcdf(int big, int small)
+{
+    int quota;
+    
+    while(small)
+    {
+        quota = big % small;
+        big = small;
+        small = quota;
+    }
+    return big;
+}
